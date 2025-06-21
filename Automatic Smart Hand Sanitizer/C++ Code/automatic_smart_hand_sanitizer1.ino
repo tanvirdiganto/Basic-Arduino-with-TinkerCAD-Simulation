8dd039072fdd48e7352b83c7aa014d88ee20c588
tanvirdiganto <tanvir2017diganto@gmail.com>
2025-06-21 23:50:44 +0600

    Update automatic_smart_hand_sanitizer1.ino

--- Automatic Smart Hand Sanitizer/C++ Code/automatic_smart_hand_sanitizer1.ino
+++ Automatic Smart Hand Sanitizer/C++ Code/automatic_smart_hand_sanitizer1.ino
@@ -1,1 +1,55 @@
+// C++ code
+//Author: Diganto 
+/* Title: Automatic Smart Hand  Sanitizer*/
+#include <Servo.h>
+Servo myservo;
+#define trigPin 2
+#define echoPin 3
+
+float duration, distance;
+
+
+void setup()
+{
+  myservo.attach(9);
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, OUTPUT);
+  
+}
+
+void loop()
+{
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2); 
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10); 
+  digitalWrite(trigPin, LOW);
+  
+  duration= pulseIn(echoPin,HIGH);
+  distance= (duration/2)*0.0349;
+  
+  Serial.print("Distance: ");
+  if(distance >=2 || distance<=4){
+    Serial.println("Out of range");
+    myservo.detach();
+  }
+  else{
+    myservo.attach(9);
+    myservo.write(0);
+    delay(1000);
+    myservo.write(90);
+    delay(1000);
+    myservo.write(180);
+    delay(1000);
+    myservo.write(90);
+  
+    Serial.print(distance);
+    Serial.println(" cm");
+    delay(500);
+    
+  }
+  delay(500);
+  myservo.detach();
+}
 
