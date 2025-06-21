711f5aaff8424c49683d7986f9a9974818c36806
tanvirdiganto <tanvir2017diganto@gmail.com>
2025-06-21 22:29:01 +0600

    Update temperature_sensor_with_arduino1.ino
    Project Title:
    Temperature Sensor with Arduino
    
    Required Components:
    Arduino board (e.g., Arduino Uno)
    
    Analog temperature sensor (e.g., LM35)
    
    Jumper wires
    
    Breadboard
    
    How It Works:
    Analog Temperature Reading:
    
    The LM35 temperature sensor provides an analog voltage corresponding to the temperature.
    
    The Arduino reads this analog voltage using its A0 pin.
    
    Voltage Conversion:
    
    The analog input (0-1024) is converted to voltage (0-5V).
    
    The voltage is then converted into Celsius using the formula:
    
    Temperature (°C)
    =
    100
    ×
    (
    Voltage
    −
    0.5
    )
    Temperature (°C)=100×(Voltage−0.5)
    The temperature is also converted into Fahrenheit using the formula:
    
    Temperature (°F)
    =
    Temperature (°C)
    ×
    9
    5
    +
    32
    Temperature (°F)=Temperature (°C)× 
    5
    9
    ​
     +32
    Displaying Results:
    
    The temperature readings in both Celsius and Fahrenheit are displayed in the Serial Monitor.
    
    The readings update every 3 seconds.
    
    This project is ideal for learning about temperature sensors, analog-to-digital conversion, and real-time data monitoring with Arduino.

--- Temperature Sensor with Arduino/C++ Code/temperature_sensor_with_arduino1.ino
+++ Temperature Sensor with Arduino/C++ Code/temperature_sensor_with_arduino1.ino
@@ -1,1 +1,51 @@
+// C++ code
+//Owner : Diganto
+//CE, KUET
 
+/*Title:Temperature Sensor with Arduino*/
+
+int tmp=A0;
+
+float volt;
+float analog_reading;
+float temp_fahrenheit;
+float temp_celcius;         
+
+void setup()
+{
+  pinMode(A0, INPUT); 
+  Serial.begin(9600);
+  
+}
+
+void loop()
+{
+  analog_reading= analogRead(A0);
+  Serial.print("\n\nAnalog readindg: "); //printing the analog input from a0 pin
+  Serial.println(analog_reading);
+  
+  
+  //conversion of analog reading to voltage 
+  volt= analog_reading*5/1024; 
+  // here , the analog reading range is from 0 to 1024 
+  // so we divided the reading by 1024 and multiplied by 5 so that the equivalent volt is gained ranging from 0 to 5 vol
+  
+  Serial.print("Voltage readindg: "); //printing the calculated voltage
+  Serial.println(volt);
+  
+  
+  //reading in celcius 
+  //10mv change in sensor per degree celcius change with 0.5V offset  
+  temp_celcius= 100*(volt-0.5);
+  
+  Serial.print("Celcius readindg: "+String(temp_celcius)+ ("C")); //printing the calculated celcius temp
+  
+  //reading in fahrenheit
+  //using C to F formula
+  temp_fahrenheit= (temp_celcius*9/5)+32;
+  
+  Serial.print("\nFahrenheit readindg: "+String(temp_fahrenheit)+ ("F")); //printing the calculated celcius temp
+  delay(3000);
+  
+  
+}
