9cc3035d0473fef81b712ae2449e42ae3a5e598e
tanvirdiganto <tanvir2017diganto@gmail.com>
2025-06-22 01:35:02 +0600

    Update traffic_light1.ino
    Project Title: Traffic Light System
    Required Components:
    Arduino board (e.g., Uno, Mega)
    
    Red LED
    
    Yellow LED
    
    Green LED
    
    Push button
    
    Resistors (220Ω for LEDs, 10kΩ for button)
    
    Breadboard & jumper wires
    
    How It Works:
    The system remains idle until the button is pressed.
    
    Upon pressing the button, the sequence starts:
    
    The red LED turns on for 2 seconds (Stop).
    
    The yellow LED lights up for 2 seconds (Get Ready).
    
    The green LED turns on for 2 seconds (Go).
    
    After completing the cycle, all LEDs turn off, and the system waits for the next button press.
    
    Working Principle:
    The push button is connected to an input pin and monitored continuously.
    
    When the button is pressed, the program executes a sequence of LED signals using digitalWrite() and delay() functions.
    
    The Serial.print() function displays messages in the Serial Monitor corresponding to each LED state.

--- Traffic Light/C++ Code/traffic_light1.ino
+++ Traffic Light/C++ Code/traffic_light1.ino
@@ -1,1 +1,47 @@
+// C++ code
+/*Diganto
+CE, KUET*/
 
+/*TITLE: Traffic Light*/
+
+int butt_state=0;
+const int butt_pin = 2;
+int Red=13;
+int Yellow=12;
+int Green=8;
+
+void setup()
+{
+  pinMode(Red, OUTPUT);
+  pinMode(Yellow, OUTPUT);
+  pinMode(Green, OUTPUT);
+  pinMode(butt_pin, INPUT);
+  Serial.begin(9600);
+  
+}
+
+void loop()
+{
+  butt_state = digitalRead(butt_pin);
+  if(butt_state==1){
+    digitalWrite(Red,1);
+    Serial.print("Stop\n");
+    delay(2000);
+    digitalWrite(Red,0);
+    delay(500);
+    
+    digitalWrite(Yellow,1);
+    Serial.print("Get Ready\n");
+    delay(2000);
+    digitalWrite(Yellow,0);
+    delay(500);
+    
+    digitalWrite(Green,1);
+    Serial.print("Bye\n");
+    delay(2000);
+    digitalWrite(Green,0);
+    delay(500);
+    
+  }
+    
+  }
