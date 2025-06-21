94866cac2bd33dd10bc930d497f1d65173a5193f
tanvirdiganto <tanvir2017diganto@gmail.com>
2025-06-22 01:48:55 +0600

    Update interfacing_photodiode_with_arduino1.ino

--- Interfacing Photodiode with Arduinoh/C++ Code/interfacing_photodiode_with_arduino1.ino
+++ Interfacing Photodiode with Arduinoh/C++ Code/interfacing_photodiode_with_arduino1.ino
@@ -1,1 +1,17 @@
+// C++ code
+//Author: Diganto
+/* Title: Interfacing Photodiode with Arduino*/
+int sensorPin = A0;
+int sensorValue = 0;
 
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(sensorPin, INPUT);
+}
+
+void loop()
+{
+  sensorValue= analogRead(sensorPin);
+  Serial.println(sensorValue);
+}
