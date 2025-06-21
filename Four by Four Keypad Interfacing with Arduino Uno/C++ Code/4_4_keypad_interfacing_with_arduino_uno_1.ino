fabe5a9c002da786dcb5cfd757ae70dad25e4dec
tanvirdiganto <tanvir2017diganto@gmail.com>
2025-06-21 22:59:45 +0600

    Update 4_4_keypad_interfacing_with_arduino_uno_1.ino

--- Four by Four Keypad Interfacing with Arduino Uno/C++ Code/4_4_keypad_interfacing_with_arduino_uno_1.ino
+++ Four by Four Keypad Interfacing with Arduino Uno/C++ Code/4_4_keypad_interfacing_with_arduino_uno_1.ino
@@ -1,1 +1,45 @@
+// C++ code
+// owner: Diganto
+// CE , KUET
 
+/*Title: 4*4 Keypad Interfacing with Arduino Uno */
+
+#include <Keypad.h>
+
+const byte ROWS=4; //keypad no of row and column
+const byte COLS=4;
+
+//keypad mapping
+char keys[ROWS][COLS]={
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+
+//pins connected with arduino 
+byte rowPins[ROWS]={9,8,7,6};
+byte colPins[COLS]={5,4,3,2};
+
+//obj of keypad class
+//converts the keys into a usable format
+Keypad kypd = Keypad(makeKeymap(keys),rowPins, colPins, ROWS, COLS);
+
+
+void setup()
+{
+  Serial.begin(9600); 
+}
+
+void loop()
+{
+  char pressedKey; //for storing the pressed key in a variable 
+
+  pressedKey= kypd.getKey(); //Get the pressed Key
+  //print the key if preesed
+  if(pressedKey){
+    Serial.print("Key pressed: ");
+    Serial.println(pressedKey);
+ 
+  }          
+}
