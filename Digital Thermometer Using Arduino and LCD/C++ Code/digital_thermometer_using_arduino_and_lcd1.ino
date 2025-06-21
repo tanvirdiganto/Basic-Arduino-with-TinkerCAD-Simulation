96306c42298e81ce82fad9db8dcd4b27e46fccd5
tanvirdiganto <tanvir2017diganto@gmail.com>
2025-06-22 00:15:20 +0600

    Update digital_thermometer_using_arduino_and_lcd1.ino

--- Digital Thermometer Using Arduino and LCD/C++ Code/digital_thermometer_using_arduino_and_lcd1.ino
+++ Digital Thermometer Using Arduino and LCD/C++ Code/digital_thermometer_using_arduino_and_lcd1.ino
@@ -1,1 +1,35 @@
+// C++ code
+//Author: Diganto
+//CE, KUET
+/* Title: Digital Thermometer Using Arduino and LCD*/
 
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12,11,5,4,3,2);
+int degree;
+double realdegree;
+
+
+void setup()
+{
+  lcd.begin(16,2);
+  degree=0;
+  realdegree=0.0;
+  lcd.print("Today's Temp: ");
+  
+}
+
+void loop()
+{
+  degree=analogRead(0);
+  realdegree=(double) degree/ 1024;
+  realdegree*=5;
+  realdegree-=0.5;
+  realdegree*=100;
+  
+  lcd.setCursor(0,1);
+  realdegree=(9.0/5)*(realdegree)+32;
+  String output= String (realdegree)+ String((char)178)+"F";
+  lcd.print(output);
+  
+}
